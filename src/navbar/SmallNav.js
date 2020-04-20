0503b60c0bdb74ebe25fe1fc2789d2a60581385c
bishopwm <bishopwm@g.cofc.edu>
2020-04-20 16:44:37 -0400

    iteration 4 -- signup

--- src/navbar/SmallNav.js
+++ src/navbar/SmallNav.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import '../navbar/SmallNav.css';
 
 class SmallNav extends Component {
     render() {
         return (
             <div>
-                I'm Small Navbar
+                <nav id="small-nav" className="navbar" role="navigation" aria-label="main navigation">
+                    <div className="navbar-brand">
+                    <h1 id="signup-heading">Signup Form</h1>
+                    <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                        <span aria-hidden="true" />
+                        <span aria-hidden="true" />
+                        <span aria-hidden="true" />
+                    </a>
+                    </div>
+                    <div id="navbarBasicExample" className="navbar-menu">
+                    <div className="navbar-start">
+                        <a className="navbar-item">
+                        Home
+                        </a>
+                        <a className="navbar-item">
+                        Documentation
+                        </a>
+                    </div>
+                    </div>
+                 </nav>
             </div>
         );
     }
